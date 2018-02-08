c0d6845313c6ef5d791471ab6d7d0ee6ffcfb242
Barta Gábor <emoryy@gmail.com>
2018-02-08 17:12:33 +0100

    return true for ds-improved-ajax + ds-pushpayload-return

--- addon/-private/features.js
+++ addon/-private/features.js
@@ -1,5 +1,8 @@
 import Ember from 'ember';
 
 export default function isEnabled() {
+  if (arguments[0] === 'ds-improved-ajax' || arguments[0] === 'ds-pushpayload-return') {
+    return true;
+  }
   return Ember.FEATURES.isEnabled(...arguments);
 }
